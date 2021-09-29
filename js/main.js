7a2cd137325ebd0000fc76b48aa554d95fefc94d
Vilija <86228325+Tamulaitiene@users.noreply.github.com>
2021-09-29 15:12:04 +0300

    Pakeitimai

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
-class ProgressBars {
-    constructor(selector, data) {
-        this.selector = selector;
-        this.data = data;
+import { ProgressBars } from "./components/progress-bar/ProgressBars.js";
+import { progressBarData } from "./data/progressBarData.js";
 
-        this.render();
-    }
-
-    render() {
-        let HTML = '';
-
-        for (let i = 0; i < this.data.length; i++) {
-            const item = this.data[i];
-            HTML += `<div class="progress-bar">
-                        <div class="top">
-                            <div class="label">${item.label}</div>
-                            <div class="value">${item.value}%</div>
-                        </div>
-                        <div class="bottom">
-                            <div class="progress" style="width: ${item.value}%;"></div>
-                        </div>
-                    </div>`;
-        }
-
-        const DOM = document.querySelector(this.selector);
-        DOM.innerHTML += HTML;
-    }
-}
-
-export { ProgressBars }
+new ProgressBars('.left-column', progressBarData);
+new Socials('footer .socials', socialsData);
